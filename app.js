36c507cd91d92c28e888cbf56ec6610d1b756e8c
talitaFesan11 <talitafesou@gmail.com>
2025-08-02 18:15:16 -0400

    deploy server local

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Bem vindos ao WebAcademy!!");
+  res.send("Bem vindos ao WebAcademy Turma 05!!");
 });
 
 app.listen(8080, () => {
